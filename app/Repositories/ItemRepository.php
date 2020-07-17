8450280863e9967917c8db8dff4ff127e8de7046
rothink <rodrigoluz@live.com>
2020-07-17 20:56:30 -0300

    Formatacao de valores

--- app/Repositories/ItemRepository.php
+++ app/Repositories/ItemRepository.php
@@ -3,6 +3,7 @@
 
 namespace App\Repositories;
 
+use App\Helper\Number;
 use App\Models\Item;
 
 class ItemRepository extends AbstractRepository
@@ -12,5 +13,16 @@ class ItemRepository extends AbstractRepository
     public function __construct(Item $model)
     {
         $this->model = $model;
+    }
+
+    public function formatParams($params)
+    {
+        $formatted = [];
+
+        if (isset($params['vl_esperado'])) {
+            $formatted['vl_esperado'] = Number::formatCurrencyBr($params['vl_esperado']);
+        }
+
+        return $formatted;
     }
 }
