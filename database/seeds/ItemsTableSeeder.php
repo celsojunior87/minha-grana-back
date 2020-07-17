75b1758c1bae66775a72218b230cdb4c3041634e
celsojunior87 <celsojunior87>
2020-07-16 22:43:50 -0300

    criacao de mais dados nas seeders

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -23,6 +23,40 @@ class ItemsTableSeeder extends Seeder
             'vl_total' => 000,
             'status' => 0,
         ]);
+        Item::create([
+            'grupo_id' => 2,
+            'nome' => 'Doacao',
+            'vl_esperado' => 0000,
+            'vl_planejado' => 0001,
+            'vl_recebido' => 000,
+            'vl_saldo_esperado' => 000,
+            'vl_saldo_realizado' => 000,
+            'vl_total' => 000,
+            'status' => 0,
+        ]);
+
+        Item::create([
+            'grupo_id' => 3,
+            'nome' => 'Despesas',
+            'vl_esperado' => 0000,
+            'vl_planejado' => 0001,
+            'vl_recebido' => 000,
+            'vl_saldo_esperado' => 000,
+            'vl_saldo_realizado' => 000,
+            'vl_total' => 000,
+            'status' => 0,
+        ]);
+        Item::create([
+            'grupo_id' => 4,
+            'nome' => 'Ajustes',
+            'vl_esperado' => 0000,
+            'vl_planejado' => 0001,
+            'vl_recebido' => 000,
+            'vl_saldo_esperado' => 000,
+            'vl_saldo_realizado' => 000,
+            'vl_total' => 000,
+            'status' => 0,
+        ]);
 
     }
 }
