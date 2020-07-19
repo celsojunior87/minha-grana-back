64bbf8bb83f6fcd5352f58e9682ed7f1722c6b07
rothink <rodrigoluz@live.com>
2020-07-19 17:00:21 -0300

    order by relation items

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -12,7 +12,7 @@ class Grupo extends Model
 
     public function items()
     {
-        return $this->hasMany(Item::class, 'grupo_id', 'id');
+        return $this->hasMany(Item::class, 'grupo_id', 'id')->orderBy('created_at');
     }
 
     /**
@@ -22,14 +22,14 @@ class Grupo extends Model
      */
     public function scopeQuery($queryBuilder, array $params)
     {
-
         if (Arr::has($params, 'date')) {
             $date = Arr::get($params, 'date');
             $newDate = Carbon::createFromFormat('Y-m', $date);
             $queryBuilder
                 ->whereBetween('data',
                     [$newDate->firstOfMonth()->format('Y-m-d'), $newDate->lastOfMonth()->format('Y-m-d')]
-                );
+                )
+                ->orderBy('created_at');
         }
         return $queryBuilder;
     }
