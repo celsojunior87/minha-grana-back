ad810e526a9268f067282ca53735387f213aac4e
celsojunior87 <celsojunior87>
2020-07-22 00:45:08 -0300

    criacao das colunas gastos e data na tabela de items

--- database/migrations/2020_07_12_175645_create_item_table.php
+++ database/migrations/2020_07_12_175645_create_item_table.php
@@ -16,12 +16,14 @@ class CreateItemTable extends Migration
         Schema::create('item', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome');
-            $table->decimal('vl_esperado',20,2)->nullable()->default(0.00);
-            $table->decimal('vl_planejado',20,2)->nullable()->default(0.0);
-            $table->decimal('vl_recebido',20,2)->nullable()->default(0.00);
-            $table->decimal('vl_saldo_esperado',20,2)->nullable()->default(0.00);
-            $table->decimal('vl_saldo_realizado',20,2)->nullable()->default(0.00);
-            $table->decimal('vl_total',20,2)->nullable()->default(0.00);
+            $table->decimal('vl_esperado', 20, 2)->nullable()->default(0.00);
+            $table->decimal('vl_planejado', 20, 2)->nullable()->default(0.0);
+            $table->decimal('vl_recebido', 20, 2)->nullable()->default(0.00);
+            $table->decimal('vl_saldo_esperado', 20, 2)->nullable()->default(0.00);
+            $table->decimal('vl_saldo_realizado', 20, 2)->nullable()->default(0.00);
+            $table->decimal('vl_total', 20, 2)->nullable()->default(0.00);
+            $table->date('data')->nullable();
+            $table->string('gasto')->nullable()->default(0.00);;
             $table->boolean('status')->nullable();
             $table->unsignedInteger('grupo_id')->nullable();
             $table->foreign('grupo_id')
