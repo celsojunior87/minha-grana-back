d55d19af0e85dded921dd6ff3a5d9f77ab043fff
celsojunior87 <celsojunior87>
2020-07-24 19:11:51 -0300

    alteracao da migration

--- database/migrations/2020_07_12_175645_create_item_table.php
+++ database/migrations/2020_07_12_175645_create_item_table.php
@@ -24,7 +24,6 @@ class CreateItemTable extends Migration
             $table->decimal('vl_total', 20, 2)->nullable()->default(0.00);
             $table->date('data')->nullable();
             $table->string('gasto')->nullable()->default(0.00);;
-            $table->unsignedInteger('status_id')->nullable();
             $table->unsignedInteger('grupo_id')->nullable();
             $table->foreign('grupo_id')
                 ->references('id')
