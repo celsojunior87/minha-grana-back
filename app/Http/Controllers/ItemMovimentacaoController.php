ffe5dd5bb9c700b7033e43840ddbd720f06b4a57
rothink <rodrigoluz@live.com>
2020-07-26 18:36:41 -0300

    Atualizadao de items

--- app/Http/Controllers/ItemMovimentacaoController.php
+++ app/Http/Controllers/ItemMovimentacaoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\ItemMovimentacaoService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 
 class ItemMovimentacaoController extends AbstractController
@@ -10,5 +12,28 @@ class ItemMovimentacaoController extends AbstractController
     public function __construct(ItemMovimentacaoService $service)
     {
         $this->service = $service;
+    }
+
+    /**
+     * Criar um item movimentacao
+     * @param $idItem
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function criarItemMovimentacao($idItem)
+    {
+        try {
+            DB::beginTransaction();
+            $response = $this->service->criarItemMovimentacao($idItem);
+            DB::commit();
+            return $this->success('Operação realizada com com sucesso', ['response' => $response]);
+        } catch (\Exception | ValidationException $e) {
+            DB::rollBack();
+            if ($e instanceof ValidationException) {
+                return $this->error('Ops', $e->errors());
+            }
+            if ($e instanceof \Exception) {
+                return $this->error($e->getMessage());
+            }
+        }
     }
 }
--- app/Services/ItemService.php
+++ app/Services/ItemService.php
@@ -9,10 +9,24 @@ use App\Repositories\ItemRepository;
 class ItemService extends AbstractService
 {
     protected $repository;
+    protected $itemMovimentacaoService;
 
-    public function __construct(ItemRepository $repository)
+    public function __construct(ItemRepository $repository, ItemMovimentacaoService $itemMovimentacaoService)
     {
         $this->repository = $repository;
+        $this->itemMovimentacaoService = $itemMovimentacaoService;
+    }
 
+    public function update($id, $data)
+    {
+        $itemMovimentacao = $this->itemMovimentacaoService
+            ->getRepository()
+            ->getModel()
+            ->firstWhere(['item_id' => $id]);
+
+        if(!$itemMovimentacao) {
+            $this->itemMovimentacaoService->save(['item_id' => $id]);
+        }
+        return parent::update($id, $data);
     }
 }
