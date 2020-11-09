f66f2fa7cf82e50a18a4b37d4ebee6421ae65285
celsojunior87 <celsojunior87>
2020-11-08 23:24:35 -0300

    alteracao de regras na transferencia

--- app/Services/ItemTransferenciaService.php
+++ app/Services/ItemTransferenciaService.php
@@ -16,16 +16,19 @@ class ItemTransferenciaService extends AbstractService
 {
     protected $repository;
     protected $itemService;
+    protected $item;
 
-    public function __construct(ItemTransferenciaRepository $repository, ItemService $itemService)
+    public function __construct(ItemTransferenciaRepository $repository, ItemService $itemService, Item $item)
     {
         $this->repository = $repository;
         $this->itemService = $itemService;
+        $this->item = $item;
 
     }
 
     public function economia($params)
     {
+
         $item = [
             'id' => $params['item_id'],
             'vl_saldo_inicial' => $params['vl_saldo_inicial'],
@@ -39,9 +42,25 @@ class ItemTransferenciaService extends AbstractService
 
     public function transferir(array $transferencia)
     {
+        $vlSaldoFinal = $this->item->getVlSaldoFinalAttribute();
         $itemDe = $this->itemService->find($transferencia['item_id_de']);
         $itemPara = $this->itemService->find($transferencia['item_id_para']);
         $vlTransferencia = $transferencia['vl_transferencia'];
+
+        if ($vlTransferencia > $vlSaldoFinal) {
+            throw new \Exception('O valor da transferencia não pode ser maior que o saldo final');
+        }else{
+            $transferir = [
+                'item_id_de' => $itemDe,
+                'item_id_para' => $itemPara,
+                'vl_transferencia' => $vlTransferencia
+            ];
+
+            $this->save($transferir);
+
+        }
+
+
 
 //       todo fazer a regra de negócio
     }
