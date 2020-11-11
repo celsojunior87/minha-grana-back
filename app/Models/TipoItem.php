b0cfe31d0986064b4cb353ec95c79605868197ad
celsojunior87 <celsojunior87>
2020-11-11 00:14:45 -0300

    alteração na tabela de item e criacao da tabela tipo de items que se relaciona com o item

---
+++ app/Models/TipoItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipoItem extends Model
+{
+    protected $table = 'tipo_item';
+    public $timestamps = false;
+
+    const ECONOMIA = 1;
+    const DIVIDAS = 2;
+}
---
+++ database/seeds/TipoItemTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\TipoItem;
+
+
+class TipoItemTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        TipoItem::create([
+            'nome'=>'Economia',
+        ]);
+        TipoItem::create([
+            'nome'=>'Dividas',
+        ]);
+    }
+}
