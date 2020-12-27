7f650c3fb087fe78603fe637cf2f1b6128af8340
celsojunior87 <celsojunior87>
2020-12-27 20:55:55 -0300

    alteracao em paleta de cores dos grupos

--- app/Helper/Color.php
+++ app/Helper/Color.php
@@ -11,10 +11,18 @@ class Color
     public static function makeRandomColor()
     {
         $color = [
-            '#EC2500',
-            '#ECE100',
-            '#EC9800',
-            '#9EDE00',
+
+            '#63E1A8',
+            '#FFCE7E',
+            '#FF8F60',
+            '#E66160',
+            '#73DBE7',
+            '#8B97DA',
+            '#E8B0CC',
+            '#D0D4CC',
+            '#F0CBAD',
+            '#95CDC0'
+
         ];
         return $color[array_rand($color)];
     }
